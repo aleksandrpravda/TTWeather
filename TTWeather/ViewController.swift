d027bf11277e2b7e13236474d8eacb747af91867
Kolya.p <kolya.p@speedandfunction.com>
2019-06-26 14:59:45 +0300

    Project created

---
+++ TTWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTWeather
+//
+//  Created by Nick on 26/06/2019.
+//  Copyright © 2019 aleksandrpravda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
